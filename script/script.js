ea43bf819c6c1e80275911c6fb5e92047cd98d8a
constant gillet <constant.gillet@hetic.net>
2019-06-19 10:29:37 +0200

    adding responsive menu

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const nav = document.querySelector('.head__navigation-menu')
-
-
+const menuButton = document.querySelector('.button-menu')
+let menuDisplayed = false
 
 document.addEventListener('scroll', function(){
     // offset++
@@ -8,4 +8,22 @@ document.addEventListener('scroll', function(){
         nav.style.opacity = 1
     }
     console.log(window.pageYOffset)
+})
+
+document.addEventListener('click', function() {
+
+    if(menuDisplayed == false) {
+
+        nav.classList.add('head__navigation-menu--displayed')
+
+        menuDisplayed = true
+
+    } else {
+
+        nav.classList.remove('head__navigation-menu--displayed') 
+
+        nav.classList.add('head__navigation-menu--hidden') 
+
+        menuDisplayed = false
+    }
 })
